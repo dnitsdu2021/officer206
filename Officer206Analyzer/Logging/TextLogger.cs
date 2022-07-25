d489b1320176d7b8f5b317dc962bb1c0de652496
Malitha Sakalasooriya <malitha92@gmail.com>
2022-07-25 16:49:25 +0530

    change itext logger

--- Officer206Analyzer/Logging/TextLogger.cs
+++ Officer206Analyzer/Logging/TextLogger.cs
@@ -6,25 +6,42 @@ using System.Web;
 
 namespace Officer206Analyzer.Logging
 {
-    public class TextLogger : ILogger
+    //public class TextLogger : ILogger
+    //{
+    //    public void Log(string v, object[] args)
+    //    {
+    //        Debug.WriteLine(v, args);
+    //        var fileName = string.Format("Log-{0}.txt", DateTime.Now.Date.ToString("dd-MM-yyyy"));
+    //        var folderPath = Path.Combine(HttpRuntime.AppDomainAppPath, "Logs");
+    //        var filePath = Path.Combine(folderPath, fileName);
+    //        if (!Directory.Exists(folderPath))
+    //            Directory.CreateDirectory(folderPath);
+            
+    //        using (var file = new StreamWriter(filePath, append: true))
+    //        {
+    //            var stringBuilder = new StringBuilder();
+    //            stringBuilder.Append(DateTime.Now);
+    //            stringBuilder.Append(" ");
+    //            stringBuilder.AppendFormat(v, args);
+    //            file.WriteLine(stringBuilder.ToString());
+    //        }
+    //    }
+    //}
+
+
+    public class DbLogger : ILogger
     {
+        private readonly AccessLog _logger;
+
+        public DbLogger(AccessLog logger)
+        {
+            _logger = logger;
+        }
+
         public void Log(string v, object[] args)
         {
-            Debug.WriteLine(v, args);
-            var fileName = string.Format("Log-{0}.txt", DateTime.Now.Date.ToString("dd-MM-yyyy"));
-            var folderPath = Path.Combine(HttpRuntime.AppDomainAppPath, "Logs");
-            var filePath = Path.Combine(folderPath, fileName);
-            if (!Directory.Exists(folderPath))
-                Directory.CreateDirectory(folderPath);
-            
-            using (var file = new StreamWriter(filePath, append: true))
-            {
-                var stringBuilder = new StringBuilder();
-                stringBuilder.Append(DateTime.Now);
-                stringBuilder.Append(" ");
-                stringBuilder.AppendFormat(v, args);
-                file.WriteLine(stringBuilder.ToString());
-            }
+            _logger.LogUserActivity(v, args);
         }
     }
+
 }
